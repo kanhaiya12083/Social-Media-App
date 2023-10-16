0bf4102a80a11af772e30cfb55e77aef232c8bd9
Kanhaiya Lal <123859716+kanhaiya12083@users.noreply.github.com>
2023-10-16 21:20:51 +0530

    Update PostShare.jsx

--- client/src/components/PostShare/PostShare.jsx
+++ client/src/components/PostShare/PostShare.jsx
@@ -85,19 +85,6 @@ const PostShare = () => {
             <UilScenery />
             Photo
           </div>
-
-          <div className="option" style={{ color: "var(--video)" }}>
-            <UilPlayCircle />
-            Video
-          </div>
-          <div className="option" style={{ color: "var(--location)" }}>
-            <UilLocationPoint />
-            Location
-          </div>
-          <div className="option" style={{ color: "var(--shedule)" }}>
-            <UilSchedule />
-            Shedule
-          </div>
           <button
             className="button ps-button"
             onClick={handleUpload}
