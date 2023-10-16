72e0a291a99c07bf9dfe5fbacd4a530682e3fc36
Kanhaiya Lal <123859716+kanhaiya12083@users.noreply.github.com>
2023-10-16 21:18:59 +0530

    Update LogoSearch.jsx

--- client/src/components/LogoSearch/LogoSearch.jsx
+++ client/src/components/LogoSearch/LogoSearch.jsx
@@ -6,12 +6,6 @@ const LogoSearch = () => {
   return (
     <div className="LogoSearch">
       <img src={Logo} alt="" />
-      <div className="Search">
-          <input type="text" placeholder="#Explore"/>
-          <div className="s-icon">
-                <UilSearch/>
-          </div>
-      </div>
     </div>
   );
 };
